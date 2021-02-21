03f0f415736f905c66413b4bfc4a42d76c89c5cf
parciffal <khachikyan.edgar@gmail.com>
2021-02-21 11:23:08 +0400

    21.2.2021

--- Python/Bot/config.py
+++ Python/Bot/config.py
@@ -1,7 +1,44 @@
+#this is bot token don't touch
 TOKEN = '988779233:AAEWzRnjmFMi7nZ60As5boVgTsnwk5zJuFk' #bot token from botfather
+#database name don't touch
 FILENAME = 'userbase.csv'
+
+#videos of Yes 
 YESVIDEO1 = 'https://youtu.be/_CNw1jj24w4'
 YESVIDEO2 = 'https://youtu.be/-3iAffptxkc'
 YESVIDEO3 = 'https://es.video.herbalife.com/detail/videos/sobre-herbalife/video/6087071760001/la-importancia-de-la-calidad?autoStart=true&sort_by=PUBLISH_DATE:DESC'
 YESVIDEO4 = 'https://youtu.be/ScZRpE6zNUc'
 YESVIDEO5 = 'https://youtu.be/lu0vjs8rNX0'
+
+#videos of No 1
+
+button1 = 'https://youtu.be/eGnS1Y3DPfU'
+button12 = 'https://youtu.be/_wvM0wIfEzY'
+button13 = 'https://youtu.be/8hkBs1BjHOk'
+button14 = 'https://youtu.be/M4EUHLVVry4'
+button15 = 'https://youtu.be/wED6-FPlhRw'
+
+#videos of No 2
+
+button21 = 'https://us02web.zoom.us/rec/share/te9YM4D6-39IRYHv2XqAX6kKL6Teaaa80HBL-_oNyE3E52U8bHjOURBoTp6ff9Ow'
+button22 = 'https://us02web.zoom.us/rec/share/z9d0f-_z2UNJUNLG4nnDBr4NRrzreaa8gyVL_fAKxBsGYAhoFVxVEF091Gk03IUS'
+button23 = 'https://youtu.be/fV4Ei7BQ_KU'
+button24 = 'https://youtu.be/IJfvR2zvM8w'
+button25 = 'https://youtu.be/vPeBWWwQESM'
+button26 = 'https://us02web.zoom.us/rec/share/u50vK-367UJObYWUxWSHfYA7NbXhaaa82iQX8qUKzEeoLMvIJrwHWuC-2_6bEwLY'
+button27 = 'https://youtu.be/ZQhU11NN1AQ'
+button28 = 'https://us02web.zoom.us/rec/share/udNWKoqr6z5LHp3m03vEYI0OGtS1T6a82ihIr_MMyxy3ivVon2ZCjy-73NZ3FJaG'
+button29 = 'https://us02web.zoom.us/rec/share/udNWKoqr6z5LHp3m03vEYI0OGtS1T6a82ihIr_MMyxy3ivVon2ZCjy-73NZ3FJaG'
+button210 = 'https://youtu.be/nlSfHw5PorI'
+
+#videos of No 3
+
+button31 = 'https://youtu.be/JCd1NAo2P7Q'
+button32 = 'https://youtu.be/GoeyMe-dlAY'
+button33 = 'https://youtu.be/Urh7ur4rUBw'
+
+#videos of No 4
+
+button41 = 'https://youtu.be/dZX_nnB3jgc'
+button42 = 'https://youtu.be/f3M3lRX2ECU'
+button43 = 'https://youtu.be/-bacP4EzaY0'
